7952d813ed275aa211a391e3e12e6abc0b59b37f
TamNhien <canhthien91@gmail.com>
2024-03-27 19:39:45 +0700

    Add project files.

---
+++ P_InPhanTuAmLonNhat.py
@@ -0,0 +1,10 @@
+﻿def find_max_negative(lst):
+    return max((num for num in lst if num < 0), default=0)
+
+def main():
+    lst = list(map(int, input("Nhập vào danh sách các số nguyên, cách nhau bởi dấu phẩy : ").split(',')))
+    print(find_max_negative(lst))
+
+if __name__ == "__main__":
+    main()
+
